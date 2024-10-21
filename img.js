6e807693095e2fab3eb5a61f57bdb589fa65eaa2
anik-sohgaura <105589950+anik-sohgaura@users.noreply.github.com>
2024-10-21 21:05:26 +0530

    Add files via upload

---
+++ img.js
@@ -0,0 +1,25 @@
+const slides=document.querySelectorAll(".slide");
+var counter=0;
+
+slides.forEach(
+    (slide, index) =>{
+        slide.style.left = `${index * 100}%`;
+    }
+);
+
+const goPrev = () =>{
+    counter--;
+    slideImg();
+}
+const goNext = () =>{
+    counter++;
+    slideImg();
+}
+
+const slideImg=()=>{
+    slides.forEach(
+        (slide)=>{
+            slide.style.transform=`translateX(-${counter * 100}%)`;
+        }
+    );
+}
